28d6f7827159d87414763e2b95e5a4afd4c73606
finnMSY <102123775+finnMSY@users.noreply.github.com>
2023-11-14 22:03:09 +1300

    Got the search working faster and debounced

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import axios from 'axios';
 import RootContext from "../../providers/root";
+import { debounce } from 'lodash';
 
 const HomePage = () => {
     const {
@@ -14,7 +15,7 @@ const HomePage = () => {
     const [searching, setSearching] = useState(false);
     const [noResults, setNoResults] = useState(false);
     const [hasSubmitted, setHasSubmitted] = useState(false);
-    const gamesPerPage = 40; 
+    const gamesPerPage = 5; 
 
     useEffect(() => {
         const fetchData = async () => {
@@ -40,41 +41,82 @@ const HomePage = () => {
     }
 
     async function getAppDetails(appIDs) {
-        const details = [];
-        for (const appID of appIDs) {
-            try {
-                const response = await axios.get(`http://localhost:8081/app_data/${appID}`);
-                if (response.data[appID].data !== undefined) {
-                    details.push(response.data[appID].data);
+        const urls = appIDs.map(item => `http://localhost:8081/app_data/${item}`);
+        const details = await Promise.all(
+            urls.map(async (url, index) => {
+                try {
+                    const response = await axios.get(url);
+                    return response.data[appIDs[index]].data;
+                } catch (error) {
+                    console.error('Error fetching data:', error);
                 }
-            } catch (error) {
-                console.error(`Error fetching data for appID ${appID}:`, error);
-            }
-        }
+            })
+        );
+
         return details;
     }
 
-    const handleOnChange = (event) => {
-        setSearching(false);
-        setNoResults(false)
-        if (event.target.value === null || event.target.value.match(/^ *$/) !== null) {
+    // function debounce(func, timeout = 500) {
+    //     let timer;
+    //     return (...args) => {
+    //         clearTimeout();
+    //         timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    //     };
+    // }
+
+    // const loadResults = async (event) => {
+    //     setSearching(true);
+    //     setNoResults(false);
+    //     setHasSubmitted(false);
+    //     const inputValue = event.target.value;
+    
+    //     if (inputValue === null || inputValue.match(/^ *$/) !== null) {
+    //         setSearchResults(null);
+    //     } else {
+    //         setSearchedName(inputValue);
+    //         const results = getAppIdByName(inputValue).slice(0, gamesPerPage + 5).sort();
+    //         const data = await getAppDetails(results);
+    //         const cleaned_data = data.filter(array => array !== undefined);
+    //         console.log(cleaned_data);
+        
+    //         if (cleaned_data.length > gamesPerPage) {
+    //             setSearchResults(cleaned_data.slice(0, gamesPerPage));
+    //         } else {
+    //             setSearchResults(cleaned_data);
+    //         }
+    //     }
+    
+    //     setSearching(false);
+    // }
+
+    const handleOnChange = debounce(async (event) => {
+        setSearching(true);
+        setNoResults(false);
+        setHasSubmitted(false);
+        const inputValue = event.target.value;
+     
+        if (inputValue === null || inputValue.match(/^ *$/) !== null) {
             setSearchResults(null);
         } else {
-            setSearchedName(event.target.value);
+            setSearchedName(inputValue);
+            const results = getAppIdByName(inputValue).slice(0, gamesPerPage + 5).sort();
+            const data = await getAppDetails(results);
+            const cleaned_data = data.filter(array => array !== undefined);
+            console.log(cleaned_data);
+     
+            if (cleaned_data.length > gamesPerPage) {
+                setSearchResults(cleaned_data.slice(0, gamesPerPage));
+            } else {
+                setSearchResults(cleaned_data);
+            }
         }
-    }
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setHasSubmitted(true)
-        setSearching(true);
-    
-        const results = getAppIdByName(searchedName).slice(0, gamesPerPage+5).sort();
-        const dataPromise = getAppDetails(results);
-        const data = await dataPromise;
-        setSearchResults(data.slice(0, gamesPerPage));
-    
+     
         setSearching(false);
+     }, 500);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setHasSubmitted(true);
     }
 
     useEffect(() => {
@@ -94,7 +136,7 @@ const HomePage = () => {
             <h1>Home Page</h1>
             <button onClick={handleClick}>Page 2</button><br />
             <form onSubmit={handleSubmit}>
-                <input placeholder='Game Name' onChange={handleOnChange}></input>
+                <input placeholder='Game Name' onKeyUp={handleOnChange}></input>
                 <button type="submit">Submit</button>
             </form>
 
